8fcab944e597ddd2a52c5122e5bc7bf79c041d15
Dimiter Geelen <mrdidel@gmail.com>
2013-04-24 14:07:19 +0200

    Test toegevoegd

---
+++ src/test/tests.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class tests {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
